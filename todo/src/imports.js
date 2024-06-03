56462b8575bdd84a11784ba5b355121746debc74
PARAS SAXENA <parassaxenaofficial@gmail.com>
2024-06-04 03:03:12 +0530

    Delete todo directory

--- todo/src/imports.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
-import { BsCheckLg } from 'react-icons/bs';
-import { Toggle } from './components/Toggle.jsx';
-import lightImage from './logo/ToDo-Logo-Yellow.png';
-import darkImage from './logo/ToDo-Logo-Black.png';
-
-export { React, useState, useEffect, AiOutlineDelete, AiOutlineEdit, BsCheckLg, Toggle, lightImage, darkImage };
