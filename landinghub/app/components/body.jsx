000446e7da3d839de9c24107f0c82428b0a31d73
PARAS SAXENA <parassaxenaofficial@gmail.com>
2024-07-04 02:46:32 +0530

    Update body.jsx

--- landinghub/app/components/body.jsx
+++ landinghub/app/components/body.jsx
@@ -7,7 +7,7 @@ const Body = () => {
 
                 <div className="flex flex-col text-center w-full mb-20">
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-                        CODXO Internship Projects
+                        <center>PARAS<br/>CODXO Internship Projects</center>
                     </h1>
                     <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
                         During my CODXO internship, I developed three key projects:
